8e064ac4fdd27695bee731805962e1f39cfbeda6
Valentin <valentin.riess@posteo.de>
2022-06-01 11:15:17 +0200

    Add test

--- hide.sh
+++ hide.sh
@@ -1,16 +1,21 @@
 #!/bin/bash
+echo "Content of the data directory:"
 ls -lah
 id
-touch text2.txt
 
 path="*"
-while getopts ":d:e:p:" opt; do
+test_mode="false"
+total_count=0
+passed_count=0
+while getopts "d:e:p:t" opt; do
   case $opt in
     d) path="$OPTARG"
     ;;
     e) ef="$OPTARG"
     ;;
     p) password="$OPTARG"
+    ;;
+    t) test_mode="true"
     ;;
     \?) echo "Invalid option -$OPTARG" >&2
     exit 1
@@ -45,9 +50,50 @@ hide()
     echo "Hiding $1"
     p_out_dir="$(dirname $1)/out"
     mkdir -p "$p_out_dir"
-    p_out="$p_out_dir/$(basename $1)"
+    p_out="$p_out_dir/$(basename $1 | sed 's/\(.*\)\..*/\1/')_out.wav"
     steghide embed -ef "$ef" -cf "$1" -p "$password" -sf "$p_out"
 }
 
-for f in $(find $path -name '*.wav'); do hide "$f"; done
+extract()
+{
+    echo "Extracting $1"
+    p_out_dir="$(dirname $1)"
+    p_xf_dir="$(dirname $1)/xf"
+    mkdir -p "$p_xf_dir"
+    p_out="$p_out_dir/$(basename $1)"
+    p_xf="$p_xf_dir/$(basename $1).txt"
+    steghide extract -sf "$p_out" -p "$password" -xf "$p_xf"
+    ((total_count++))
+}
+
+test()
+{
+    echo "Testing $1"
+    p_xf_dir="$(dirname $1)/xf"
+    p_xf="$p_xf_dir/$(basename $1).txt"
+    if cmp -s "$p_xf" "$ef"
+    then
+      echo "Test passed."
+      ((passed_count++))
+    else
+      echo "Test failed."
+    fi
+    rm -r "$p_xf_dir"
+}
+
+if [ "$test_mode" == "true" ]
+then
+  for f in $(find $path -name '*_out.wav')
+  do
+    extract "$f"
+    test "$f"
+  done
+  echo -e "\n====\n$passed_count of $total_count files passed the test."
+else
+  for f in $(find $path -name '*.wav')
+  do
+    hide "$f"
+  done
+fi
+
 
